c2db323c1ebc84c98b80b91903d5fba56a15c28b
Mathias <mat.micheli99@gmail.com>
2025-04-08 23:04:28 +0200

    Add api to get all benches

--- src/lib/components/BenchCard.svelte
+++ src/lib/components/BenchCard.svelte
@@ -1,32 +1,52 @@
 <script lang="ts">
 	import { Share } from '@lucide/svelte';
 	import Slider from '$lib/components/Slider.svelte';
+	import { onMount } from 'svelte';
+	import type { Bench } from '@prisma/client';
+
+	let benches: Bench[] = $state([]);
+
+	const getBenches = async () => {
+		const response = await fetch('/api/bench');
+		benches = await response.json();
+	};
+
+	onMount(() => {
+		getBenches();
+	});
 </script>
 
-<div class="mx-auto max-w-md overflow-hidden rounded-2xl border border-gray-200 bg-white shadow-md">
-	<div class="relative">
-		<Slider />
-		<span class="absolute top-2 left-2 rounded-full bg-white px-2 py-1 text-sm text-gray-700 shadow"
-			>En vedette ✨</span
-		>
-		<button class="absolute top-2 right-2 rounded-full bg-white p-1 shadow">
-			<Share size={16} />
-		</button>
-	</div>
-
-	<div class="p-4">
-		<div class="mb-1 flex items-center text-sm text-gray-500">
-			<span class="font-semibold text-green-600">● Ouvert</span>
-			<span class="mx-2">•</span>
-			<span>320 m</span>
+{#each benches as bench, i}
+	<div
+		class="mx-auto max-w-lg my-6 overflow-hidden rounded-2xl border border-gray-200 bg-white shadow-md"
+	>
+		<div class="relative">
+			<Slider />
+			{#if i === 0}
+				<span
+					class="absolute top-2 left-2 rounded-full bg-white px-2 py-1 text-sm text-gray-700 shadow">En vedette ✨</span>
+			{/if}
+			<button class="absolute top-2 right-2 rounded-full bg-white p-1 shadow">
+				<Share size={16} />
+			</button>
 		</div>
+		<div class="p-4">
+			<div class="mb-1 flex items-center text-sm text-gray-500">
+				<span class="font-semibold text-green-600">● Ouvert</span>
+				<span class="mx-2">•</span>
+				<span>320 m</span>
+			</div>
 
-		<h2 class="text-lg font-semibold text-gray-900">Banc des taulards, Créteil</h2>
-		<p class="text-sm text-gray-500">132 Avenue de la Prison, 94000 Créteil, France</p>
+		<h2 class="text-lg font-semibold text-gray-900">{bench.title}, Paris</h2>
+		<p class="text-sm text-gray-500">{bench.address}</p>
 
 		<div class="mt-3 flex flex-wrap gap-2">
-			<span class="rounded-full bg-pink-100 px-3 py-1 text-sm text-pink-800">Date 🌸</span>
-			<span class="rounded-full bg-green-100 px-3 py-1 text-sm text-green-800">Accès Gratuit</span>
+			{#each bench.tags as tag}
+					<span
+						class="rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-700"
+					>{tag.name}</span
+					>
+			{/each}
 			<button class="rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-700"
 				>Voir les plages horaires</button
 			>
@@ -35,3 +55,4 @@
 		<div class="mt-3 text-sm font-medium text-gray-700">★ 4,68</div>
 	</div>
 </div>
+{/each}
---
+++ src/routes/api/bench/+server.ts
@@ -0,0 +1,13 @@
+import { json } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+export async function GET() {
+	const benches = await prisma.bench.findMany({
+		include: {
+			tags: true,
+		},
+	})
+
+	return json(benches)
+}
